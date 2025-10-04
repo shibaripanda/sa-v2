5726d2200fc431ab0c55a50c08a26330d398ca4c
Panda <prevetlunatikam@gmail.com>
2025-10-04 17:01:37 +0300

    last1

--- client-user/vite.config.ts
+++ client-user/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+// Получаем __dirname в ES-модуле
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Загружаем конкретный env-файл
+dotenv.config({ path: path.resolve(__dirname, '../envs/.env.client-user-dev') });
+
 export default defineConfig({
-  plugins: [react()],
-})
+  // теперь переменные из файла доступны
+  // в клиентском коде нужны с VITE_*
+});
