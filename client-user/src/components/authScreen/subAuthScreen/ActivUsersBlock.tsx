dbec5fd6bea180a16df3f5b2cecdc1fc819ce5cc
shibaripanda <102354922+shibaripanda@users.noreply.github.com>
2025-10-22 17:48:15 +0300

    max

--- client-user/src/components/authScreen/subAuthScreen/ActivUsersBlock.tsx
+++ client-user/src/components/authScreen/subAuthScreen/ActivUsersBlock.tsx
@@ -1,8 +1,9 @@
-import { Button, Divider, Grid, Space } from "@mantine/core"
+import { Box, Button, Divider, Grid, Space } from "@mantine/core"
 import { googleLogout } from "@react-oauth/google";
 import { AuthScreenInterface } from "../mainScreen/AuthScreen";
 import { IconLogout } from "@tabler/icons-react";
 import { User } from "../../../interfaces/user";
+import React from "react";
 
 interface ServiceMod extends AuthScreenInterface {
     setServiseModal: any;
@@ -11,7 +12,7 @@ interface ServiceMod extends AuthScreenInterface {
 export function ActivUsersBlock(props: ServiceMod) {
 
     const showIndividualButExit = (item: User) => {
-        if (props.loginedUsers.length > 1) {
+        // if (props.loginedUsers.length > 1) {
             return (
             <Grid.Col span={props.loginedUsers.length > 1 ? 3 : 0}><Button
                 key={item._id}
@@ -31,28 +32,32 @@ export function ActivUsersBlock(props: ServiceMod) {
             </Button>
             </Grid.Col>
             )
-        }
+        // }
     }
     
     if(props.loginedUsers.length)
         return (
             <div>
             <Grid justify="space-between" align="center" gutter={10}>
-            {props.loginedUsers.map(item => <><Grid.Col key={item._id} span={props.loginedUsers.length > 1 ? 9 : 12}><Button
-                key={item._id}
-                color='green'
-                fullWidth
-                mt="xs" 
-                size="md"
-                onClick={async () => {
-                    props.pickUser(item)
-                    props.setServiseModal.open()
-                }}
-                >
-                {item.name ? item.name : item.email}
-            </Button></Grid.Col>
-            {showIndividualButExit(item)}
-            </>)}
+            {props.loginedUsers.map(item =>
+            <React.Fragment key={item._id}> 
+                <Grid.Col span={props.loginedUsers.length > 1 ? 9 : 12}>
+                    <Button
+                        color='green'
+                        fullWidth
+                        mt="xs" 
+                        size="md"
+                        onClick={async () => {
+                            props.pickUser(item)
+                            props.setServiseModal.open()
+                        }}
+                        >
+                        {item.name ? item.name : item.email}
+                    </Button>
+                </Grid.Col>
+                {props.loginedUsers.length > 1 ? showIndividualButExit(item) : <></>}
+            </React.Fragment>
+            )}
             </Grid>
             <Button
                 color='red'
